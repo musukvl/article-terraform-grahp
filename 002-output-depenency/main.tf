0e57cc7bafc189338d38bfb010a0ac78ba9fd3d4
Alexey Ryazhskikh <musukvl@gmail.com>
2023-04-02 01:47:07 +0300

    Resource group -> Storage account example

---
+++ 002-output-depenency/main.tf
@@ -0,0 +1,17 @@
+locals {
+  location = "northeurope"
+  resource_group_name = "ary-graph-example-rg"   
+}
+
+resource "azurerm_resource_group" "graph_example_rg" {
+  name     = local.resource_group_name
+  location = local.location
+}
+
+resource "azurerm_storage_account" "storage_account" {
+  name                     = "arygrexstacc"
+  resource_group_name      = azurerm_resource_group.graph_example_rg.name
+  location                 = local.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
